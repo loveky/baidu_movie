4c0c8ef5614cd5ff1fb5c547eda02df83cb2f93b
loveky <ylzcylx@gmail.com>
2013-04-01 16:28:34 +0800

    initial drop

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BaiduMovie::Application.config.secret_token = 'f887bd304c42b4456e20e60c14f40bcbefcba8d8444d9d26acbf9fc7aa4fceedc84f2ac67380fbda16e8955b093be7e04cb817210cb0d0272213260326f68889'
