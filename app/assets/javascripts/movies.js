c837f47c51f9da7807ebf77646272f60bc57055f
loveky <ylzcylx@gmail.com>
2013-04-21 18:26:33 +0800

    make navagation bar center

---
+++ app/assets/javascripts/movies.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    // make navagation bar align center
+    $("div.pagination ul").css("margin-left", ($("div.pagination").width() - $("div.pagination ul").width()) / 2 + "px" );
+});
+
