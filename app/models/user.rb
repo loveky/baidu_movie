f21c4211c2ca99eb3d9a4ce6845d68a05175f739
loveky <ylzcylx@gmail.com>
2013-04-21 22:13:31 +0800

    use chinese in validate message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
+#encoding: utf-8
 class User < ActiveRecord::Base
     attr_accessible :username, :password
-    validates :username, :password, :presence => true
-    validates :password, :length => { :minimum => 6 }
+    validates :username, :password, :presence => { :message => "不能为空" }
+    validates :password, :length => { :minimum => 6, :message => "最少为6位" }
 
 
     has_secure_password
