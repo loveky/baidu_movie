cde5c7133d7037d5d660b452060114968ee722dc
loveky <ylzcylx@gmail.com>
2013-04-21 21:34:44 +0800

    add register validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
-  attr_accessible :username, :password
+    attr_accessible :username, :password
+    validates :username, :password, :presence => true
+    validates :password, :length => { :minimum => 6 }
 
-  has_secure_password
+
+    has_secure_password
 end
