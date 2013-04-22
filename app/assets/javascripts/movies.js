3f25e42eb7dc4d98646c51ba579acd71336febb1
loveky <ylzcylx@gmail.com>
2013-04-22 23:11:25 +0800

    add search box to side bar

--- app/assets/javascripts/movies.js
+++ app/assets/javascripts/movies.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
     // make navagation bar align center
     $("div.pagination ul").css("margin-left", ($("div.pagination").width() - $("div.pagination ul").width()) / 2 + "px" );
+
+    // redirect to search result page when click search button
+    $("button#search").click(function() {
+        window.location = window.location.origin + "/search/" + $("#search").siblings("input").val()
+    });
 });
 
