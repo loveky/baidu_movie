26fdd9ad50960b49cc2032bee36d91754094ba40
loveky <ylzcylx@gmail.com>
2013-04-22 17:46:45 +0800

    add password confirmation when register

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 #encoding: utf-8
 class User < ActiveRecord::Base
-    attr_accessible :username, :password
-    validates :username, :password, :presence => { :message => "不能为空" }
-    validates :password, :length => { :minimum => 6, :message => "最少为6位" }
+    attr_accessible :username, :password, :password_confirmation
+    validates :username, :password, :password_confirmation, :presence => { :message => "不能为空" }
+    validates :password, :length => { :minimum => 6, :message => "最少为6位" }, :unless => Proc.new { |a| a.password.blank? }
+    validates :password, :confirmation => {:message => "两次密码输入不一致"}, :unless => Proc.new { |a| a.password.blank? || a.password_confirmation.blank? }
     validates :username, :uniqueness => {:case_sensitive => false, :message => "已被使用，再选个名字吧"}
 
     has_secure_password
