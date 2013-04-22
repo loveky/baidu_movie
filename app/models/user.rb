76a1b49318804dc7277acff95ffdc2fc92ad1228
loveky <ylzcylx@gmail.com>
2013-04-22 17:29:02 +0800

    add uniqueness validate on username when register

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
     attr_accessible :username, :password
     validates :username, :password, :presence => { :message => "不能为空" }
     validates :password, :length => { :minimum => 6, :message => "最少为6位" }
-
+    validates :username, :uniqueness => {:case_sensitive => false, :message => "已被使用，再选个名字吧"}
 
     has_secure_password
 end
