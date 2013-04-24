1a4014c8dc4d324d3d7994af381edf41644a7ec5
loveky <ylzcylx@gmail.com>
2013-04-24 21:09:35 +0800

    add category movie count on category page

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,5 +1,8 @@
 class CategoryController < ApplicationController
     def show
         @movies = Movie.paginate(:page => params[:page] ? params[:page] : 1).find_all_by_category(params[:category_name])
+        if params[:page].nil?
+            @category_total_count = Movie.find_all_by_category(params[:category_name]).size
+        end
     end
 end
